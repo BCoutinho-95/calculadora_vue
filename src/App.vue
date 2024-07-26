4926eac2f39b160c746c63dccb9d86fd8dc5dcfd
BCoutinho-95 <bcoutinhoti@gmail.com>
2024-07-26 09:23:48 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <h1>Calculadora Aritmética</h1>
+    <div>
+      <input type="number" v-model.number="num1" placeholder="Número 1" />
+      <select v-model="operacao">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" v-model.number="num2" placeholder="Número 2" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <h2>Resultado: {{ resultado }}</h2>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: '+',
+    };
+  },
+  computed: {
+    resultado() {
+      let res = 0;
+      switch (this.operacao) {
+        case '+':
+          res = this.num1 + this.num2;
+          break;
+        case '-':
+          res = this.num1 - this.num2;
+          break;
+        case '*':
+          res = this.num1 * this.num2;
+          break;
+        case '/':
+          res = this.num2 !== 0 ? this.num1 / this.num2 : 'Erro: Divisão por zero';
+          break;
+      }
+      return res;
+    },
+  },
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+input, select {
+  margin: 5px;
 }
 </style>
